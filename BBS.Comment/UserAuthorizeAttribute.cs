948b7fd7c25552be975835b1aaedc5023eb4b5bf
zhangchenghang <chenghang1937@126.com>
2015-06-26 09:23:37 +0800

    提交

---
+++ BBS.Comment/UserAuthorizeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BBS.Model;
+using System.Web.Routing;
+
+namespace System.Web.Mvc
+{
+    public class UserAuthorizeAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            MemberInfo user = (MemberInfo)filterContext.HttpContext.Session["member"];
+            if (user == null)// || filterContext.HttpContext.Session.Mode != 0)
+            {
+                string s = filterContext.RequestContext.HttpContext.Request.Url.ToString();
+                //filterContext.Result = new RedirectResult("/Admin/SysUser/Login");
+                filterContext.Result = new RedirectToRouteResult(new RouteValueDictionary { { "action", "Login" }, { "controller", "Home" }, { "returnurl", s } });
+            }
+            return;
+        }
+    }
+}

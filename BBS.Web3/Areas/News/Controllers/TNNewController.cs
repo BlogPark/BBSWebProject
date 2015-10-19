6dd874708848c7114d04c9a0e42784f32eb0ba22
zhangchenghang <chenghang1937@126.com>
2015-10-20 07:14:03 +0800

    cv

---
+++ BBS.Web3/Areas/News/Controllers/TNNewController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBS.Web3.Areas.News.Controllers
+{
+    public class TNNewController : Controller
+    {
+        //
+        // GET: /News/TNNew/
+
+        /// <summary>
+        /// 新闻列表页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult NewsDetail()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ BBS.Web3/Controllers/PublicController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BBS.Web3.Controllers
+{
+    public class PublicController : Controller
+    {
+        //
+        // GET: /Public/
+
+        public ActionResult Header()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Footer()
+        {
+            return PartialView();
+        }
+
+        public ActionResult Reagier()
+        {
+            return View();
+        }
+    }
+}
